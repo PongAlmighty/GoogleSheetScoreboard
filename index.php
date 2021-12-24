9b9431a1b6caa5b06e0af3d07630c824d37fbcc8
mrjones-plip <mrjones-plip@plip.com>
2021-12-23 23:31:33 -0800

    rewrite from scratch, break into files, only show leaderboard

--- index.php
+++ index.php
@@ -1,52 +1,20 @@
-<?PHP
-$bots=Array();
-$botsScore=Array();
-$getData=$_GET['refresh'] . "";
-if($getData=='true') {
-  $getData=1;
-}
-// JF: Put your Google Sheets sheet_id here
-// JF: Make Sure the permissions allow anyone with link
-// 
-if($getData==1) {
-  //$sheet_id="1nCbksR7m-vftkQpCrJsUxkhmqoU5pFcQmNDwvIInGps";
-    $sheet_id = "19WYCoDTrvYsb-oF-xTIKSvIyOHyNkT4bm0DcoMrXY_o"; $sheet_name = "Sheet1";
-  $tmp=$_GET['id'] . "";
-  if($tmp>"") { $sheet_id=$tmp;}
-  $tmp=$_GET['name'] . "";
-  if($tmp>"") { $sheet_id=$tmp;}
-  $url = "https://docs.google.com/spreadsheets/d/";
-  $url .= $sheet_id . "/gviz/tq?tqx=out:json&sheet=";
-  $url .= $sheet_name;
-  $contents=file_get_contents($url);
-  $contents= substr($contents, 47, -1);
-  $contents=substr($contents,0,strlen($contents)-1);
-  file_put_contents('cache.json',$contents);
-}
-
-$contents=file_get_contents('cache.json');
-$jsonData=json_decode($contents,true);
-$rows=$jsonData['table']['rows'];
-foreach($rows as $k=>$v) {
-  $playerName=$v['c'][0]['v'];
-  $playerScore=$v['c'][1]['v'];
-  $bots[]=$playerName;
-  $botsScore[]=$playerScore;
-}
-
-$data=Array('Bot Name'=>$bots,'Tokens'=>$botsScore,'url'=>$url);
-$contents=json_encode($data);
-$contents="var json=" . $contents . ";" ;
-file_put_contents('players.js',$contents);
-$display=$_GET['display'] . "";
-if($display=='leaderboard') {
-  include('LeaderBoard.html');
-  exit();
-}
-if($display=='top3') {
-  include('top3.html');
-  exit();
-}
-include('test.html');
-
-?>
+<?php require_once('config.php'); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family='Press Start 2P'">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+        <script src="players.js"></script>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="leaderboard">
+        </div>
+        <script>
+            loadData();
+            setInterval(loadData, <?php echo $refreshRate ?>000);
+        </script>
+    </body>
+</html>
