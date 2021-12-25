fe3a99c06fa27bfa62a92fed4db42215a2fde10f
VKettmann <vkettmann@gmail.com>
2021-12-25 05:24:20 +0000

    it verks!

--- players.js
+++ players.js
@@ -18,19 +18,25 @@ function getRow(total, count, name, score){
     let cssStyle = ' style="font-size: 10px;" '
     if (score > 0) {
         
-        cssStyle = ' style="font-size:' +  (60) + 'px;" '
+        cssStyle = ' style="font-size:' +  (20) + 'px;" '
         // no, the parens aren't needed but it's so I can put a variable back later
         // safety: cssStyle = ' style="font-size:' +  (800 * (score / total)) + 'px;" '
     
     }
     
     if (count == 1) {
-      return '<tr class="trNum01" ' + cssStyle + '><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td class="trNum01" id = "plrName">'+ name +'</td><td id = "plrScore">'+ score +'</td></tr>';
+      return '<tr class="trNum01"><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td  id = "plrName">'+ name +'</td><td  id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
+    } else if (count == 2) {
+      return '<tr class="trNum02"><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td  id = "plrName">'+ name +'</td><td  id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
+    } else if (count == 3) {
+      return '<tr class="trNum03"><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td  id = "plrName">'+ name +'</td><td  id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
+    } else if (count == 4) {
+      return '<tr class="trNum04"><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td  id = "plrName">'+ name +'</td><td  id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
+    } else if (count == 5) {
+      return '<tr class="trNum05"><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td  id = "plrName">'+ name +'</td><td  id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
+    } else {
+      return '<tr  ' + cssStyle + '><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td id = "plrName">'+ name +'</td><td id = "plrScore">'+ score +'</td><td id="tdSpacer">&nbsp</td></tr>';
     }
-    
-    // if count > 3 do this
-    return '<tr  ' + cssStyle + '><td id="plrRnk">' + count + ')</td><td id="tdSpacer">&nbsp</td><td id = "plrName">'+ name +'</td><td id = "plrScore">'+ score +'</td></tr>';
-    
 }
 
 function loadData() {
